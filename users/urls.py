10ca18864c554699738c292787bae9731b442d22
Sohret Aga <shohretaga@gmail.com>
2024-11-10 23:01:23 +0400

    auth logout

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from . import views
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout')
+    path('', views.AuthView.as_view(), name='auth'),
+    path('login', views.CustomLoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(next_page='/'), name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.views import LoginView
+from django.views.generic import TemplateView
+from .mixins import LogoutRequiredMixin
 
 # Create your views here.
 
-class CustomLoginView(LoginView):
+class AuthView(LogoutRequiredMixin, TemplateView):
     template_name = 'authenticate.html'
 
+class CustomLoginView(LoginView):
     def get_redirect_url(self) -> str:
         next_url = self.request.GET.get('next')
         if next_url:

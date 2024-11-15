7f203ef8cbbf64ae76c35bd84d97fd9b815b6bcf
Sohret Aga <shohretaga@gmail.com>
2024-11-16 03:36:45 +0400

    profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
+    whatsapp = models.CharField(max_length=15, blank=True, null=True)
+    telegram = models.CharField(max_length=32, blank=True, null=True)
+    instagram = models.CharField(max_length=30, blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
